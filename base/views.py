ddc2f2cdf643aa67c8f5180c56b53015670f643e
Edmar Lelis Loureço Júnior <120860142+EdmarLelis@users.noreply.github.com>
2023-03-08 17:01:20 -0300

    discstudy start rooms

--- base/views.py
+++ base/views.py
@@ -3,10 +3,19 @@ from django.http import HttpRequest
 
 # Create your views here.
 
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':1, 'name':'Web Develop'},
+    {'id':1, 'name':'Learn C#'},
+]
+
+
+
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'home.html', context)
 
 
-def rooms(request):
+def room(request):
     return render(request, 'room.html')
